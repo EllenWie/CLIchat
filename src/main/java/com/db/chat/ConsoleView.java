bedd83d2857b3bf414f82696bfdc47d8a0cb913e
ZhenyaMsl <>
2018-08-31 17:05:03 +0300

    Do work.

---
+++ src/main/java/com/db/chat/ConsoleView.java
@@ -0,0 +1,21 @@
+package com.db.chat;
+
+import java.util.ArrayList;
+
+public class ConsoleView implements View {
+
+    @Override
+    public void send() {
+
+    }
+
+    @Override
+    public void display(Message message) {
+
+    }
+
+    @Override
+    public void displayHistory(ArrayList<Message> history) {
+
+    }
+}
