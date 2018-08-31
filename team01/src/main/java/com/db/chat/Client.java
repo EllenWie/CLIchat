5869ee3ba7d8911394aeea9213e9c5732afa522a
ZhenyaMsl <>
2018-08-31 15:52:00 +0300

    added gitignore and client send and receive

--- team01/src/main/java/com/db/chat/Client.java
+++ team01/src/main/java/com/db/chat/Client.java
@@ -5,8 +5,22 @@ import java.util.ArrayList;
 public class Client {
     private Server server;
 
+    public Client(Server server) {
+        this.server = server;
+        server.connect(this);
+    }
+
     public ArrayList<Message> getHistory(){
         return server.getHistory();
+    }
+
+    public int send(Message message) {
+        server.receive(message);
+        return 0;
+    }
+
+    public int receive(Message message) {
+        return 0;
     }
 
 }
