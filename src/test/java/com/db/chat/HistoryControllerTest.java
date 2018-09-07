a55d030000866fd099c52b90152155dd93b43151
ElenaRozhkova <rse.elena.rus@gmail.com>
2018-09-07 12:22:15 +0300

    Update Test for HistoryController

--- src/test/java/com/db/chat/HistoryControllerTest.java
+++ src/test/java/com/db/chat/HistoryControllerTest.java
@@ -1,16 +1,24 @@
 package com.db.chat;
 
-import org.junit.Test;
+import org.junit.*;
 
 import java.io.File;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
 
 import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertEquals;
 
 public class HistoryControllerTest {
+    @BeforeClass
+    public static void init() {
+        File toBeDeleted = new File(".", "testFile.txt");
+        if (toBeDeleted.exists()) {
+            toBeDeleted.setWritable(true);
+            toBeDeleted.delete();
+        }
+    }
+
     @Test
     public void shouldReturnCorrectListOfMessagesWhenGetHistory() throws HistoryControllerException, InterruptedException {
         HistoryController historyController = new HistoryController("testFile.txt");
@@ -18,7 +26,6 @@ public class HistoryControllerTest {
         historyController.addMessage(testMessage);
         sleep(2000);
         List<Message> actualMessageList = historyController.getHistory();
-//        new File("testFile.txt").delete();
         Message actualMessage = actualMessageList.get(0);
 
         assertEquals(testMessage.getText(), actualMessage.getText());
