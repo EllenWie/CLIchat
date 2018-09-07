02c95bbf9ee8d3fd3f321e2d6cc2db3d56dd2434
ZhenyaMsl <GITasdf4567>
2018-09-07 16:52:09 +0300

    consoleview refactor.

--- src/main/java/com/db/chat/client/view/ConsoleView.java
+++ src/main/java/com/db/chat/client/view/ConsoleView.java
@@ -11,44 +11,88 @@ import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * ConsoleView is an object that provides
+ * access to console.
+ * Its run function goes in the loop
+ * listening to console input.
+ * It stops after /quit command received.
+ * It provides method to print Messages
+ * to console with formating.
+ */
 public class ConsoleView implements View {
-    private Client client;
+    transient private Client client;
 
+    /**
+     * Assigns this object to the client object
+     * that uses it.
+     * It is necessary to get access to client's method
+     * that sends messages through socket.
+     * @param client
+     */
+    @Override
     public void setClient(Client client) {
         this.client = client;
     }
 
+    /**
+     * This method is getting string message
+     * and handle it.
+     * After parsing message this function
+     * invokes appropriate client's methods
+     * @param currentMessage
+     */
     @Override
     public void send(String currentMessage) {
         String[] arguments = currentMessage.split(" ", 2);
         String type = arguments[0];
         switch (type) {
             case "/snd":
-                if (nickIsSet()) {
-                    String text = arguments.length > 1 ? arguments[1].trim() : "";
-                    if (text.length() > 0) {
-                        client.send(new Message(null, text, MessageType.MESSAGE, client.getNick()));
-                    } else {
-                        display(new Message(null, "Empty message. Was not sent to server.", MessageType.ERROR, client.getNick()));
-                    }
-                }
+                handleSendMessage(arguments);
                 break;
             case "/hist":
-                if (nickIsSet()) {
-                    client.send(new Message(null, null, MessageType.HISTORY, client.getNick()));
-                }
+                handleHistCommand();
                 break;
             case "/chid":
-                String newNick = arguments.length > 1 ? arguments[1].trim() : "";
-                if (newNick.length() > 0) {
-                    client.setNick(newNick);
-                    System.out.println("Your nick was set" + System.lineSeparator());
-                } else {
-                    display(new Message(null, "Nick must not be empty", MessageType.ERROR, client.getNick()));
-                }
+                handleChidCommand(arguments);
                 break;
             default:
                 display(new Message(null, "Wrong command", MessageType.ERROR, client.getNick()));
+        }
+    }
+
+    private void handleChidCommand(String[] arguments) {
+        String newNick = getCommandData(arguments);
+        if (newNick.length() > 0) {
+            client.setNick(newNick);
+            System.out.println("Your nick was set" + System.lineSeparator());
+        } else {
+            display(new Message(null, "Nick must not be empty", MessageType.ERROR, client.getNick()));
+        }
+    }
+
+    private String getCommandData(String[] arguments) {
+        if (arguments.length > 1) {
+            return arguments[1].trim();
+        } else {
+            return "";
+        }
+    }
+
+    private void handleHistCommand() {
+        if (nickIsSet()) {
+            client.send(new Message(null, null, MessageType.HISTORY, client.getNick()));
+        }
+    }
+
+    private void handleSendMessage(String[] arguments) {
+        if (nickIsSet()) {
+            String text = getCommandData(arguments);
+            if (text.length() > 0) {
+                client.send(new Message(null, text, MessageType.MESSAGE, client.getNick()));
+            } else {
+                display(new Message(null, "Empty message. Was not sent to server.", MessageType.ERROR, client.getNick()));
+            }
         }
     }
 
@@ -60,6 +104,16 @@ public class ConsoleView implements View {
         return true;
     }
 
+    /**
+     * Displaying function.
+     * It displays message in the console
+     * depending on message's type.
+     * It can display user messages in format:
+     * Date [author] message
+     * and error messages in format:
+     * Error: message
+     * @param message
+     */
     @Override
     public void display(Message message) {
         switch (message.getType()) {
@@ -70,9 +124,20 @@ public class ConsoleView implements View {
             case ERROR:
                 System.out.println("Error: " + message.getText() + System.lineSeparator());
                 break;
+            default:
+                System.out.println("Error: wrong message type");
         }
     }
 
+    /**
+     * main cycle of ConsoleView object.
+     * it runs in the loop listening
+     * to user's input until getting
+     * /quit command.
+     * It has fixed thread pool to handle
+     * every message it has read from console
+     * in different thread.
+     */
     @Override
     public void run() {
         ExecutorService pool = Executors.newFixedThreadPool(2);
@@ -81,17 +146,21 @@ public class ConsoleView implements View {
                         new BufferedInputStream(System.in)))) {
             while (!Thread.interrupted()) {
                 String currentMessage = bufferedReader.readLine();
-                if ("/quit".equals(currentMessage.split(" ", 2)[0])) {
-                    client.quit();
-                } else {
-                    pool.execute(() -> {
-                        send(currentMessage);
-                    });
-                }
+                handleCommand(pool, currentMessage);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         pool.shutdownNow();
+    }
+
+    private void handleCommand(ExecutorService pool, String currentMessage) {
+        if ("/quit".equals(currentMessage.split(" ", 2)[0])) {
+            client.quit();
+        } else {
+            pool.execute(() -> {
+                send(currentMessage);
+            });
+        }
     }
 }
